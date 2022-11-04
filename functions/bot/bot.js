96818953bd00d07c223a4951b46eef069133d8ee
Coding Kiran <codingkiran92@gmail.com>
2022-11-04 18:50:33 +0530

    testing start function with new message

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -5,7 +5,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start(ctx => {
   console.log("Received /start command")
   try {
-    return ctx.reply("Hi")
+    return ctx.reply(`Hello ${ctx.from.first_name} ${ctx.from.last_name}. Please send the mobile number with country code.`)
   } catch (e) {
     console.error("error in start action:", e)
     return ctx.reply("Error occured")
