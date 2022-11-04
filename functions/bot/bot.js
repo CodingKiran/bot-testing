7cd6c53b8119e936e1e5865375957849de5c933e
Coding Kiran <codingkiran92@gmail.com>
2022-11-04 19:22:17 +0530

    deleted some code to check for functionality

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -17,15 +17,14 @@ bot.hears(Number, (ctx) => {
   try {
     if (ctx.message.text.length === 13) {
       return ctx.reply(
-        "Ok",
-        Markup.inlineKeyboard([
+        Markup.inlineKeyboard(
           Markup.button.url(
             JSON.stringify(ctx.from.first_name),
             `https://api.whatsapp.com/send?phone=+${JSON.stringify(
               parseInt(ctx.message.text)
             )}`
-          ),
-        ])
+          )
+        )
       );
     } else {
       return ctx.reply(
